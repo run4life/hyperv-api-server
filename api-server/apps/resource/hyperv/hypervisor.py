92e56d89b5380b87bf54d4855f9e03b3a1907818
zhaojf <zhaojf_xt@teamsun.com.cn>
2020-03-25 16:05:03 +0800

    add

--- api-server/apps/resource/hyperv/hypervisor.py
+++ api-server/apps/resource/hyperv/hypervisor.py
@@ -2,9 +2,12 @@ import wmi
 
 class HypervisorClient(object):
     
-    def get_wmi_client(self, server, user, password, namespace):
-        conn = wmi.connect_server(server=server, user=user, password=password, namespace=namespace)
-        client = wmi.WMI(wmi=conn)
+    def get_wmi_client(self, server, user, password):
+#        if namespace:
+#            conn = wmi.connect_server(server=server, user=user, password=password, namespace=namespace)
+#        else:
+        conn = wmi.connect_server(server=server, user=user, password=password)
+        client = wmi.WMI(wmi=conn) 
         return client
     
     def _get_cpu_info(self,client):
@@ -37,10 +40,11 @@ class HypervisorClient(object):
         return mem_info
     
     def get_hypervisor_list(self):
-        client = self.get_wmi_client("172.30.126.56", "administrator", "passw0rd#!1", r"root\virtualization\v2")
+#        client = self.get_wmi_client("172.30.126.56", "administrator", "passw0rd#!1", r"root\virtualization\v2")
 #        vms_obj = client.query("select Name,ElementName,Status,EnabledState from Msvm_ComputerSystem")
 #        for vm in vms_obj:
 #            print vm
+        client = self.get_wmi_client("172.30.126.56", "administrator", "passw0rd#!1")
         cpu = self._get_cpu_info(client)
         print cpu
         memory = self._get_memory_info(client)
