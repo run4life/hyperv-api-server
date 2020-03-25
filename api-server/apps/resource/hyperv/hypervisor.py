5ae3694579d64c016042200acd8370844b1965f9
zhaojf <zhaojf_xt@teamsun.com.cn>
2020-03-25 15:15:32 +0800

    add

--- api-server/apps/resource/hyperv/hypervisor.py
+++ api-server/apps/resource/hyperv/hypervisor.py
@@ -1,13 +1,26 @@
 import wmi
+from SimpleXMLRPCServer import server
+from MySQLdb.constants.ER import USERNAME
 
 
 
 
 class HypervisorClient(object):
-
-    def get_hypervisor_list(self):
-        conn = wmi.connect_server(server="172.30.126.56", user="administrator", password="passw0rd#!1", namespace=r"root\virtualization\v2")
+    
+    def get_wmi_client(self, server, user, password, namespace):
+        conn = wmi.connect_server(server=server, user=user, password=password, namespace=namespace)
         client = wmi.WMI(wmi=conn)
+        return client
+    
+    def _get_cpu_info(self):
+        pass
+    
+    def _get_memory_info(self):
+        pass
+    
+    
+    def get_hypervisor_list(self):
+        client = get_wmi_client("172.30.126.56", "administrator", "passw0rd#!1", r"root\virtualization\v2")
         vms_obj = client.query("select Name,ElementName,Status,EnabledState from Msvm_ComputerSystem")
         for vm in vms_obj:
             print vm
