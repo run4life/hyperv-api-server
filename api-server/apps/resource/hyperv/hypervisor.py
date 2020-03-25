bd2bdff40eecb863ed302f92a47489c4c253e672
zhaojf <zhaojf_xt@teamsun.com.cn>
2020-03-25 15:29:21 +0800

    add

--- api-server/apps/resource/hyperv/hypervisor.py
+++ api-server/apps/resource/hyperv/hypervisor.py
@@ -7,15 +7,43 @@ class HypervisorClient(object):
         client = wmi.WMI(wmi=conn)
         return client
     
-    def _get_cpu_info(self):
-        pass
+    def _get_cpu_info(self,client):
+        cpus = client.query(
+            "SELECT Architecture, Name, Manufacturer, MaxClockSpeed, "
+            "NumberOfCores, NumberOfLogicalProcessors FROM Win32_Processor "
+            "WHERE ProcessorType = 3")
+        cpus_list = []
+        for cpu in cpus:
+            cpu_info = {'Architecture': cpu.Architecture,
+                        'Name': cpu.Name,
+                        'Manufacturer': cpu.Manufacturer,
+                        'MaxClockSpeed': cpu.MaxClockSpeed,
+                        'NumberOfCores': cpu.NumberOfCores,
+                        'NumberOfLogicalProcessors':
+                        cpu.NumberOfLogicalProcessors}
+            cpus_list.append(cpu_info)
+        print cpus_list
     
-    def _get_memory_info(self):
-        pass
+    def _get_memory_info(self, client):
+
+        mems = client.query("SELECT TotalVisibleMemorySize, "
+                                          "FreePhysicalMemory "
+                                          "FROM win32_operatingsystem")[0]
+        mems_list = []
+        
+        mem_info = {'TotalVisibleMemorySize': mem_info.TotalVisibleMemorySize,
+                    'FreePhysicalMemory': mem_info.FreePhysicalMemory}
+        return mem_info
+        
+        
     
     
     def get_hypervisor_list(self):
         client = self.get_wmi_client("172.30.126.56", "administrator", "passw0rd#!1", r"root\virtualization\v2")
-        vms_obj = client.query("select Name,ElementName,Status,EnabledState from Msvm_ComputerSystem")
-        for vm in vms_obj:
-            print vm
+#        vms_obj = client.query("select Name,ElementName,Status,EnabledState from Msvm_ComputerSystem")
+#        for vm in vms_obj:
+#            print vm
+        cpu = self._get_cpu_info(client)
+        print cpu
+        memory = self._get_memory_info(client)
+        print memory
